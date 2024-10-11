ecf710b4f62d42edb729db12f07fe3868e404bab
Arshpreet Singh <loloflol1223@gmail.com>
2024-10-11 22:06:07 +0530

    add various routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,21 +1,65 @@
-import express from "express";
+import { Router } from "express";
 import {
-    uploadVideo,
+    deleteVideo,
     getAllVideos,
     getVideoById,
+    publishAVideo,
+    togglePublishStatus,
+    updateVideo,
 } from "../controllers/video.controller.js";
-import upload from "../middleware/multer.js"; // Multer config for file upload
-import { authMiddleware } from "../middleware/authMiddleware.js"; // Protect routes if necessary
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
-const router = express.Router();
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-// Route for uploading video with file upload and Cloudinary processing
-router.post("/upload", authMiddleware, upload.single("video"), uploadVideo);
+router
+    .route("/")
+    .get(getAllVideos)
+    .post(
+        upload.fields([
+            {
+                name: "videoFile",
+                maxCount: 1,
+            },
+            {
+                name: "thumbnail",
+                maxCount: 1,
+            },
+        ]),
+        publishAVideo
+    );
 
-// Get all videos (paginated)
-router.get("/", getAllVideos);
+router
+    .route("/:videoId")
+    .get(getVideoById)
+    .delete(deleteVideo)
+    .patch(upload.single("thumbnail"), updateVideo);
 
-// Get video by ID
-router.get("/:id", getVideoById);
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
 
 export default router;
+
+
+
+// import express from "express";
+// import {
+//     uploadVideo,
+//     getAllVideos,
+//     getVideoById,
+// } from "../controllers/video.controller.js";
+// import upload from "../middleware/multer.js"; // Multer config for file upload
+// import { authMiddleware } from "../middleware/authMiddleware.js"; // Protect routes if necessary
+
+// const router = express.Router();
+
+// // Route for uploading video with file upload and Cloudinary processing
+// router.post("/upload", authMiddleware, upload.single("video"), uploadVideo);
+
+// // Get all videos (paginated)
+// router.get("/", getAllVideos);
+
+// // Get video by ID
+// router.get("/:id", getVideoById);
+
+// export default router;
